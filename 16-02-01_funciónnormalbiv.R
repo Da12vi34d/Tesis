902fdda33fc69850d125d58ccadfd1f02ca46fb1
Juan Carlos Martínez-Ovando <jc.martinez.ovando@gmail.com>
2016-04-07 14:48:29 -0500

    Alta de documentos desde JCMO

---
+++ 16-02-01_funciónnormalbiv.R
@@ -0,0 +1,29 @@
+% evalua una función normal bivariada dada una matriz nx2 de datos, un vector de medias 
+y una matreiz de varianza-covarianza %
+
+Normal<-function(X,M,S){
+  
+  
+  x<-(1/sqrt(2*pi*abs(det(S))))*exp(-(1/2)*(X-M)%*%solve(S)%*%(X-M))
+  x
+}
+
+
+%crea una malla para graficar una superficioe normal bivariada%
+GN<-function(t,r,M,S){
+  j=1
+  
+  
+  n<-length(t)
+ 
+  Z<-matrix(seq(1,n*n),ncol=n)
+  
+  for(j in 1:n){
+    i=1  
+    for (i in 1:n) {
+                          
+           Z[i,j]<-Normal(c(t[i],r[j]),M,S)
+                      }          
+              }
+  Z
+}

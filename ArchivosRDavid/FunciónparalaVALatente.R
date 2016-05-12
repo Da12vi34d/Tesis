c0415d473f0b8b8239d789d052ec0917e342ef0b
Juan Carlos Martínez-Ovando <jc.martinez.ovando@gmail.com>
2016-05-12 18:06:56 -0500

    new code david

---
+++ ArchivosRDavid/FunciónparalaVALatente.R
@@ -0,0 +1,19 @@
+VariableLatente
+lat<-function(x,rr,mmm,bb,ss,ll,ff,p){
+  bb<-x*bb
+  ss<-solve(x*ss)
+  A<-(rr-mmm-bb)%*%ss%*%(rr-mmm-bb)
+  x<-exp(-.5*A)*x^{-.5*p-1.5}*exp(-(x-ff)^{2}/(2*x*ff^{2}))
+  
+  if (x >0) 
+    x<-log(x,base=exp(1))
+  else
+    x<- -Inf
+  
+  x
+}
+
+latente<-function(x){
+  x<-lat(x,rr,mmm,bb,ss,ll,ff,p)
+  x
+  }
